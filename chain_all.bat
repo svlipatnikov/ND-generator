08208d2ce4e9bcaf6a82e2f126d265181b1ee0d2
Seregy <sv.lipatnikov@gmail.com>
2022-08-13 00:07:26 +0400

    add vl2ports generation

--- chain_all.bat
+++ chain_all.bat
@@ -1,9 +1,11 @@
 RMDIR /S /Q delivery
 
-node index.js
+node index-gen-nd.js
 
 CALL chain.bat DU1
 CALL chain.bat DU2
 CALL chain.bat DU3
 CALL chain.bat DU4
 CALL chain.bat DU5
+
+node index-gen-x2p.js
